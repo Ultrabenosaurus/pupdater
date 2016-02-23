e8573a7f84960c285dc94d5c1add9e5ca401439c
Ultrabenosaurus <naebeth@googlemail.com>
2016-02-23 19:59:49 +0000

    can now customise text output

--- Updater.py
+++ Updater.py
@@ -1,4 +1,3 @@
-
 import urllib2, json
 
 #
@@ -10,32 +9,54 @@ import urllib2, json
 #
 # example:
 # import Updater
-# upd = Updater.New("1.2","Ultrabenosaurus/EasyXdcc")
+# upd = Updater.New("Ultrabenosaurus/EasyXdcc", "1.2")
 # print upd.Simple()
 # print upd.Main()
 #
 
 class Updater:
 
-    # ver       current version of script
-    # repo      GitHub repo to query for update
-    # text      text prompt to return for alerting the user, set to false/blank to return nothing
-    #           use $ as a placeholder for the latest version number in your custom message
-    def __init__(self, ver, repo, text=None):
+    # @required
+    # repo          GitHub repo to query for update
+    # ver           current version of script
+    #
+    # @optional
+    # new_full      long string to return for alerting the user of an available update
+    # new_short     short string to return for alerting the user of an available update
+    #           use $latest$ as a placeholder for the LATEST VERSION in your custom message
+    #           use $current$ as a placeholder for the CURRENT VERSION in your custom message
+    #           use $repo$ as a placeholder for the REPO NAME in your custom message
+    # no_new        string to return if no newer version found
+    def __init__(self, repo, ver, **kwargs):
         self.ver = ver
         self.repo = repo
-        if text is None:
-            self.text = "Version $ of " + repo + " is now available, you are running " + ver + ". Please visit https://github.com/" + repo + "/releases/tag/$ for more information."
+
+        if not kwargs.has_key("new_full"):
+            self.new_full = "Version $latest$ of $repo$ is now available, you are running $current$. Please visit https://github.com/$repo$/releases/tag/$latest$ for more information."
         else:
-            self.text = text
+            self.new_full = kwargs["new_full"]
+
+        if not kwargs.has_key("new_short"):
+            self.new_short = "New version available from https://github.com/$repo$/releases/tag/$latest$"
+        else:
+            self.new_short = kwargs["new_short"]
+
+        if not kwargs.has_key("no_new"):
+            self.no_new = "You are running the latest version available."
+        else:
+            self.no_new = kwargs["no_new"]
+
         self.new = False
 
     # return messages to tell users the latest version
-    def Main(self):
+    def Main(self, which="full"):
         if self.isOld():
-            return self.text.replace("$", self.new)
+            if "full" == which:
+                return self.prepString(self.new_full)
+            else:
+                return self.prepString(self.new_short)
         else:
-            return "You are running the latest version available."
+            return self.prepString(self.no_new)
 
     # return True if there's a newer version, otherwise False
     def Simple(self):
@@ -58,6 +79,10 @@ class Updater:
         else:
             return False
 
+    # parse the output strings to replace the symbols
+    def prepString(self, string):
+        return string.replace("$latest$", str(self.new)).replace("$current$", str(self.ver)).replace("$repo$", self.repo)
+
 # because apparently python doesn't like using classes for importable modules
-def New(ver, repo, text=None):
-    return Updater(ver, repo, text)
+def New(ver, repo, **kwargs):
+    return Updater(ver, repo, **kwargs)
--- test.py
+++ test.py
@@ -1,6 +1,23 @@
 import Updater
 
-print "testing against: 1.2"
-upd = Updater.New("1.2", "Ultrabenosaurus/EasyXdcc")
-print upd.Simple()
-print upd.Main()
+print "default output"
+upd = Updater.New("Ultrabenosaurus/EasyXdcc", "1.2")
+print "SIMPLE:\n\t%s" % upd.Simple()
+print "NEW-FULL:\n\t" + upd.Main()
+print "NEW-SHORT:\n\t" + upd.Main("short")
+
+upd = Updater.New("Ultrabenosaurus/EasyXdcc", "9.9")
+print "NO-NEW:\n\t" + upd.Main()
+
+print "\ncustom output"
+upd = Updater.New("Ultrabenosaurus/EasyXdcc", "1.2",
+    new_short="A new version of $repo$ is available.",
+    new_full="Please visit https://github.com/$repo$/releases/tag/$latest$ for the latest version."
+)
+print "SIMPLE:\n\t%s" % upd.Simple()
+print "NEW-FULL:\n\t" + upd.Main()
+print "NEW-SHORT:\n\t" + upd.Main("short")
+upd = Updater.New("Ultrabenosaurus/EasyXdcc", "9.9",
+    no_new="Your version $current$ is the newest available."
+)
+print "NO-NEW:\n\t" + upd.Main()
